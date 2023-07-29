efa7f0cd3c318c91ee96d7f0492218ff730880a1
SaarthakMaini <saarthakmaini@gmail.com>
2023-07-29 21:25:41 +0530

    :star: <feature> Add My Tasks Functionality

--- script.js
+++ script.js
@@ -1,61 +1,71 @@
+let isRunning = false
+let isPaused = false
+let interval,minutes,seconds;
 
-  let isRunning = false
-  let isPaused = false
-  let interval
 
-  const timerDisplay = document.getElementById('timer-display')
-  const startBtn = document.getElementById('start-btn')
-  const pauseBtn = document.getElementById('pause-btn')
-  const resetBtn = document.getElementById('reset-btn')
-  const audio = new Audio('audio/alarm-clock-short-6402.mp3')
+const timerDisplay = document.getElementById('timer-display')
+const startBtn = document.getElementById('start-btn')
+const pauseBtn = document.getElementById('pause-btn')
+const resetBtn = document.getElementById('reset-btn')
+const addBtn = document.getElementById('add-btn')
+const audio = new Audio('audio/alarm-clock-short-6402.mp3')
 
-  startBtn.addEventListener('click', () => {
-	if (!isRunning) {
-	  startTimer()
-	  isRunning = true
-	}
-  })
+startBtn.addEventListener('click', () => {
+if (!isRunning) {
+  startTimer()
+  isRunning = true
+}
+})
 
-  pauseBtn.addEventListener('click', () => {
-	if (isRunning) {
-	  if (isPaused) {
-		interval = setInterval(() => {
-		  startTimer(1)
-		}, 1000)
-		pauseBtn.textContent = 'Pause'
-		isPaused = false
-		audio.play();
-	  } else {
-		clearInterval(interval)
-		pauseBtn.textContent = 'Resume'
-		isPaused = true
-		audio.pause();
-	  }
-	}
-  })
-
-  resetBtn.addEventListener('click', () => {
-	clearInterval(interval)
-	timerDisplay.textContent = '00:00'
-	pauseBtn.textContent = 'Pause'
-	isRunning = false
-	isPaused = false
-  })
-
-  function startTimer() {
-	let totalSeconds = 0
+pauseBtn.addEventListener('click', () => {
+if (isRunning) {
+  if (isPaused) {
 	interval = setInterval(() => {
-	  const minutes = Math.floor(totalSeconds / 60)
-	  const seconds = totalSeconds % 60
-	  const displayMinutes = minutes < 10 ? '0' + minutes : minutes
-	  const displaySeconds = seconds < 10 ? '0' + seconds : seconds
-	  if(seconds==5){
-		clearInterval(interval);
-		timerDisplay.textContent = "Time To Exercise!";
-		audio.play();
-		return;
-	  }
-	  timerDisplay.textContent = `${displayMinutes}:${displaySeconds}`
-	  totalSeconds++
+	  startTimer(1)
 	}, 1000)
+	pauseBtn.textContent = 'Pause'
+	isPaused = false
+	audio.play();
+  } else {
+	clearInterval(interval)
+	pauseBtn.textContent = 'Resume'
+	isPaused = true
+	audio.pause();
   }
+}
+})
+
+resetBtn.addEventListener('click', () => {
+clearInterval(interval)
+timerDisplay.textContent = '00:00'
+pauseBtn.textContent = 'Pause'
+isRunning = false
+isPaused = false
+})
+
+addBtn.addEventListener('click',() => {
+	const liElement = document.createElement('li')
+	liElement.className = "list"
+	let inputText = document.getElementById("mytasks").value;
+	liElement.textContent = inputText
+	const container = document.getElementById('task')
+	container.appendChild(liElement)
+})
+
+function startTimer() {
+let totalSeconds = 0
+interval = setInterval(() => {
+  minutes = Math.floor(totalSeconds / 60)
+  seconds = totalSeconds % 60
+  let displayMinutes = minutes < 10 ? '0' + minutes : minutes
+  let displaySeconds = seconds < 10 ? '0' + seconds : seconds
+  if(seconds==5){
+	clearInterval(interval);
+	timerDisplay.textContent = "Time To Exercise!";
+	audio.play();
+	return;
+  }
+  timerDisplay.textContent = `${displayMinutes}:${displaySeconds}`
+  totalSeconds++
+}, 1000)
+}
